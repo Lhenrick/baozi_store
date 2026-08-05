d86476417bdc1204c847f9bb095ec4a10c07174a
Luan Henrique <luanhenriquez10@hotmail.com>
2026-08-05 00:52:45 -0300

    service methods added

--- src/main/java/com/luan/baozi_store/controller/ClienteController.java
+++ src/main/java/com/luan/baozi_store/controller/ClienteController.java
@@ -1,62 +1,41 @@
 package com.luan.baozi_store.controller;
 
 import com.luan.baozi_store.model.Cliente;
-import com.luan.baozi_store.repository.ClienteRepository;
-import org.apache.coyote.Response;
+import com.luan.baozi_store.service.ClienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    private final ClienteRepository clienteRepository;
+    private final ClienteService clienteService;
 
-    public ClienteController(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     @GetMapping
-     public List<Cliente> findAll(){
-        return clienteRepository.findAll();
+    public List<Cliente> findAll(){
+        return clienteService.findAll();
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> findById(@PathVariable Long id){
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-
-       if(cliente.isPresent()){
-
-           return ResponseEntity.ok(cliente.get());
-
-        }
-
-       return ResponseEntity.notFound().build();
+        return clienteService.findById(id);
     }
 
     @PostMapping
     public ResponseEntity<Cliente> saveCliente(@RequestBody Cliente cliente){
-         Cliente clienteSalvo = clienteRepository.save(cliente);
-
-         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clienteSalvo.getId()).toUri();
-
-         return ResponseEntity.created(location).body(clienteSalvo);
+         return clienteService.saveCliente(cliente);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
-        if(clienteRepository.existsById(id)){
-            clienteRepository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.notFound().build();
+        return clienteService.deleteById(id);
 
     }
 
--- src/main/java/com/luan/baozi_store/service/ClienteService.java
+++ src/main/java/com/luan/baozi_store/service/ClienteService.java
@@ -1,4 +1,60 @@
 package com.luan.baozi_store.service;
 
+import com.luan.baozi_store.model.Cliente;
+import com.luan.baozi_store.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> findAll(){
+        return clienteRepository.findAll();
+    }
+
+
+    public ResponseEntity<Cliente> findById(@PathVariable Long id){
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+
+        if(cliente.isPresent()){
+
+            return ResponseEntity.ok(cliente.get());
+
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> saveCliente(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clienteRepository.save(cliente);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clienteSalvo.getId()).toUri();
+
+        return ResponseEntity.created(location).body(clienteSalvo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        if(clienteRepository.existsById(id)){
+            clienteRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
 }
